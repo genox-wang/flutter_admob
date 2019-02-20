e2e1d9a4dd342bb46cda45676e175a74a273cafc
wangji <genius840215@163.com>
2019-02-20 11:14:02 +0800

    first commit

---
+++ ios/Classes/FlutterAdmobPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterAdmobPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/FlutterAdmobPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterAdmobPlugin.h"
+
+@implementation FlutterAdmobPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_admob"
+            binaryMessenger:[registrar messenger]];
+  FlutterAdmobPlugin* instance = [[FlutterAdmobPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
